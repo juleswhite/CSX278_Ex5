c90a7ed3059167d3c96702f924ffe5530ffe272c
Joseph Stahl <josephst18+git@outlook.com>
2016-09-20 11:56:41 -0500

    attendance test

---
+++ src/test/java/org/magnum/cs278/testdriven/SortAttendanceTest.java
@@ -0,0 +1,41 @@
+package org.magnum.cs278.testdriven;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Joseph on 9/20/2016.
+ */
+public class SortAttendanceTest {
+
+    private App app = new App();
+    // sortAttendance is descending order (largest first)
+    List<Event> sortedList = app.sortAttendance();
+
+    @Test
+    public void testLargestAttendance() throws Exception {
+        Event topEvent = sortedList.get(0);
+        assertEquals((Integer.parseInt(topEvent.getAttendance())), 284000);
+    }
+
+    @Test
+    public void testLeastAttendance() throws Exception {
+        Event leastEvent = sortedList.get(sortedList.size() - 1);
+        assertEquals(Integer.parseInt(leastEvent.getAttendance()), 0);
+    }
+
+    @Test
+    public void testIncremental() throws Exception {
+        int randomPos = (int) (Math.random() * sortedList.size()) - 2;
+        Event largerEvent = sortedList.get(randomPos);
+        Event smallerEvent = sortedList.get(randomPos + 1);
+        assertTrue(Integer.parseInt(largerEvent.getAttendance()) >= Integer.parseInt(smallerEvent.getAttendance()));
+    }
+
+}
