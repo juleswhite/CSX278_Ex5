e2fdd08c1589263bc3836d80ebd6623184fe1bed
Arjun Dubashi <adubashi@gmail.com>
2016-09-20 12:02:13 -0500

    used lambdas

--- src/main/java/org/magnum/cs278/testdriven/App.java
+++ src/main/java/org/magnum/cs278/testdriven/App.java
@@ -88,18 +88,7 @@ public class App {
 	
 	//Filter by location 
 	
-	public List<Event> filterByLocation(String location) throws Exception {
-		List<Event> events = this.getParkSpecialPermits();
-		
-		List<Event> eventsForLocation = new ArrayList<Event>();
-		
-		for(Event event: events){
-			if(event.getLocation() == location){
-				eventsForLocation.add(event);
-			}
-			
-		}
-		
-		return eventsForLocation;
+	public List<Event> filterByLocation(final String location) throws Exception {
+		return this.getParkSpecialPermits().stream().filter(e -> e.getLocation() == location ).collect(Collectors.toList());
 	}
 }
